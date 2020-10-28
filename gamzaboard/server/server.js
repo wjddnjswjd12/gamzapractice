102a7721f2735936727568a0e86afda41e9f6968
이정원 <wjddnjswjd12@gmail.com>
2020-10-28 19:49:31 +0900

    first commit

---
+++ gamzaboard/server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+
+app.get('/api/host', (req, res) => {
+    res.send({ host: 'jwl' });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server On: http://localhost:${PORT}/`);
+})
---
+++ gamzaboard/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      host: '',
+    }
+
+  }
+  componentDidMount() {
+    this._getHost();
+  }
+
+  _getHost = async () => {
+    const res = await axios.get('/api/host');
+    this.setState({ host: res.data.host })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h3> Welcome to <u>{this.state.host}</u>Market!</h3>
+      </div>
+
+    )
+  }
+}
+export default App;
